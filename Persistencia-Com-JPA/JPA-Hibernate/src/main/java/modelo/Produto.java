bd7b57d5d6fc7b2e1b31bb7e616381e41eb21320
Ramon Fernandes Viana <ramon.fv95@gmail.com>
2023-05-11 15:15:10 -0300

    feat: Mapeando uma entidade

---
+++ Persistencia-Com-JPA/JPA-Hibernate/src/main/java/modelo/Produto.java
@@ -0,0 +1,52 @@
+package modelo;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+//@Entity  indica que essa classe é uma entidade da JPA
+//@Table, utiliza quando o nome da tabela no banco de dados é diferente do nome da classe
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
